6f8fbfb814c7a584ed72f6312bc0d93a82444ddb
witaloxz <witalodias1@gmail.com>
2025-08-06 21:45:28 -0300

    Lesson, Content, Task

---
+++ back-end/src/main/java/com/witalo/learn/entities/Content.java
@@ -0,0 +1,41 @@
+package com.witalo.learn.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serial;
+@Entity
+@Table(name = "tb_content")
+public class Content extends Lesson{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String textContent;
+    private String videoUri;
+
+    public Content(){
+
+    }
+
+    public Content(Long id, String title, Integer position, Section section, String textContent, String videoUri) {
+        super(id, title, position, section);
+        this.textContent = textContent;
+        this.videoUri = videoUri;
+    }
+
+    public String getTextContent() {
+        return textContent;
+    }
+
+    public void setTextContent(String textContent) {
+        this.textContent = textContent;
+    }
+
+    public String getVideoUri() {
+        return videoUri;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+}
